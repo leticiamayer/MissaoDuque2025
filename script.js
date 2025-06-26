90f9e05e80bd5b440e05f4eae5d25dff5c1bd6e6
leticiamayer <160251420+leticiamayer@users.noreply.github.com>
2025-06-26 11:12:33 -0300

    site

---
+++ script.js
@@ -0,0 +1,6 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultados = document.querySelector(".texto-resultado")
+
